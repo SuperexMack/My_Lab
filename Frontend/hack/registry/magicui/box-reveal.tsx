650d8f84b835a0e9e419874003b1cc8fbf5c79ec
SuperexMack <unilexerox@gmail.com>
2025-03-06 16:37:21 +0530

    Fixed Bugs

--- Frontend/hack/registry/magicui/box-reveal.tsx
+++ Frontend/hack/registry/magicui/box-reveal.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import React, { ReactNode } from "react";
 import { motion, useAnimation, useInView } from "motion/react";
 import { useEffect, useRef } from "react";
 
 interface BoxRevealProps {
-  children: JSX.Element;
+  children: ReactNode;
   width?: "fit-content" | "100%";
   boxColor?: string;
   duration?: number;
